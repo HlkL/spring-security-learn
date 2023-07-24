2f1c44e89f43eaa8091e4153ef60b8b6f57d714f
HG <hglkl6854@gmail.com>
2023-07-24 23:21:34 +0800

    add: jwt依赖

--- security-learn/src/main/java/com/hg/securitylearn/SecurityLearnApplication.java
+++ security-learn/src/main/java/com/hg/securitylearn/SecurityLearnApplication.java
@@ -1,12 +1,18 @@
 package com.hg.securitylearn;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * @author hougen
+ */
+@Slf4j
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SecurityLearnApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecurityLearnApplication.class, args);
+        log.info("start success");
     }
 }
